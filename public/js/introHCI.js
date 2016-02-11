e2433c10adbc9124a6e02b520f0f5c14b0ae1ee4
bhaddad5 <bhaddad5@gmail.com>
2016-02-10 22:54:06 -0800

    lab done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,8 +25,14 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	
+	$.get("/project/"+idNumber, callback);
 
 	console.log("User clicked on project " + idNumber);
+}
+
+function callback(result) {
+	$("#project"+result.id).find(".details").html("<h1>"+result.title+"</h1> <h3>"+result.date+"</h3>"+result.summary+"<img src="+result.image+" alt='Lorem Pixel image' class='detailsImage'>");
 }
 
 /*
@@ -35,4 +41,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", paletteCallback);
+}
+
+function paletteCallback(result) {
+	console.log(result);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
